cbc6c51464df664618286e0f7b4700b28078eeaf
ashley <wilson@neuro.duke.edu>
2014-03-19 14:51:59 -0400

    added code for incorporation of locomotion into imaging data

--- ashley/basic2Pscript_bodies_loc.m
+++ ashley/basic2Pscript_bodies_loc.m
@@ -1,6 +1,6 @@
 %% load MWorks file
 
-mworks = 'data-i004-140306-1743';
+mworks = 'data-i023-140317-1616';
 load (mworks);
 
 %% Parameters
@@ -13,9 +13,9 @@ nOFF = 150./down;
 nStim = 8;
 Az = [-30];
 El = [15];
-Dirs = [0 45 90 135 180 225 270 315];
-date = '140302';
-mouse = 'G004';
+Dir = [0 45 90 135 180 225 270 315];
+date = '140317';
+mouse = 'G023';
 %% Sum-up locomotion data for each trial and calculate speed (Stim ON only)
 
 %create a matrix including all counters
@@ -34,21 +34,13 @@ trialon_timeS = input.nScansOn./frame_rate;
 trialon_avgspeed_mat = trial_totalloc./trialon_timeS;
 
 %% Set threshold for locomotion and sort trials as running and not running
-trialon_run = find(trialon_avgspeed_mat>0.5);
-trialon_stat = find(trialon_avgspeed_mat<0.5);
-trialon_whichrun = zeros(size(trialon_avgspeed_mat));
-trialon_whichrun(trialon_run) = 1;
-
-%% create indicies for stimulus type
-nRep = 5;
-stim_mat = zeros(nStim,nRep,(nON+nOFF));
-start = 1;
-for iRep = 1:nRep
-    for iStim = 1:nStim   
-        stim_mat(iStim,iRep,:) = 1+((iStim-1)*(nON+nOFF))+((iRep-1)*((nON+nOFF)*nStim)): nON + nOFF + ((iStim-1)*(nON+nOFF))+((iRep-1)*((nON+nOFF)*nStim));
-    end
-    start= start+nON+nOFF;
-end
+trialNumbers = 1:length(trialon_avgspeed_mat);
+trialon_run = (trialon_avgspeed_mat>4);
+runTrials = find(trialon_run);
+trialon_LocMatrix = [trialNumbers', trialon_avgspeed_mat',trialon_run'];
+trialon_norun = (trialon_avgspeed_mat<4);
+norunTrials = find(trialon_norun);
+trialon_whichrun = trialon_avgspeed_mat(trialon_run);
 
 %% load 2P imaging data
 data = readrawfile;
@@ -58,6 +50,7 @@ data = readrawfile;
 
 data_down = stackGroupProject(data,down);
 clear data
+
 
 %remove negative data (by addition)
 data_sub = data_down-min(min(min(data_down,[],1),[],2),[],3);
@@ -91,6 +84,12 @@ dFoverF_data = bsxfun(@rdivide, dF_data, nOFF_avg);
 max_dF = max(dFoverF_data,[],3);
 figure; imagesq(max_dF); colormap(gray)
 
+%% add on indices to locomotion matrix
+for itrial = 1:(nStim*nRep)
+    nON_ind_firsts(itrial) = nON_ind(1+(nON*(itrial-1)));
+end
+trialon_LocMatrix = [trialNumbers', trialon_avgspeed_mat',trialon_run',nON_ind_firsts'];
+
 %% use max dF/F to find ROIS
 
 bwout = imCellEditInteractive(max_dF);
@@ -111,4 +110,42 @@ for iRep = 1:nRep
     start= start+nON+nOFF;
 end
 
-%sort by locomotion/stationary
+%plot data
+for iCell = 3;
+    figure;
+    for iStim = 1:nStim
+        subplot(2,3,iStim)
+        rep_mat = zeros(nON+nOFF,nRep);
+        for iRep = 1:nRep
+            plot(1-nOFF:nON,data_TC(squeeze(stim_mat(iStim,iRep,:))',iCell), 'k');
+            hold on
+            rep_mat(:,iRep) = data_TC(squeeze(stim_mat(iStim,iRep,:))',iCell);
+        end
+        plot(1-nOFF:nON, mean(rep_mat,2), 'r');
+        hold on
+        ylim([min(data_TC(:,iCell),[],1) max(data_TC(:,iCell),[],1)])
+        stim_Az = rem(iStim,size(Az,2));
+        if stim_Az == 0
+            stim_Az = size(Az,2);
+        end
+        stim_El= ceil(iStim./size(Az,2));
+        title(['Az = ' num2str(Az(1,stim_Az)) ' El = ' num2str(El(1,stim_El))]);
+    end
+end
+%% find start off and on frames for locomotion pulses
+data_TC
+sitm_mat
+trialon_avgspeed_mat
+stim_mat_locperrep
+stim_mat_locperon
+%find locomotion for each stim type and each repeat
+stim_mat_locperrep = zeros(nStim,1,nRep);
+for iRep = 1:nRep
+    for iStim = 1:nStim   
+        stim_mat_locperrep(iStim,:,iRep) = trialon_avgspeed_mat(iStim + (nStim*(iRep-1)));
+    end
+end
+
+stim_mat_locperon = zeros((nStim*nRep),nON);
+
+%1+((iStim-1)*(nON+nOFF))+((iRep-1)*((nON+nOFF)*nStim)): nON + nOFF + ((iStim-1)*(nON+nOFF))+((iRep-1)*((nON+nOFF)*nStim));
