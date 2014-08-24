8f59f315511ceaa4c9498091864353314245130b
ashley <wilson@neuro.duke.edu>
2014-08-24 10:59:03 -0400

    working on Flashing Stim 2P analysis

--- ashley/flashing stim analysis/FlashingStimAnalysis.m
+++ ashley/flashing stim analysis/FlashingStimAnalysis.m
@@ -17,6 +17,11 @@ clear data_reg
 %load dataset and mworks file with 'Load_SBXdataset_fast.m'
 edit Load_SBXdataset_fast.m
 
+%remove negative data (by addition)
+data_sub = data-min(min(min(data,[],1),[],2),[],3);
+data = data_sub
+clear data_sub
+
 [out data_reg] = stackRegister(data, data_avg);
 clear data
 
@@ -52,21 +57,21 @@ nOFF_ind = nOFF_ind(:,1:siz-1);
 %F=average fluorescence for all iti
 F1 = mean(data_reg(:,:,nOFF_ind),3);
 
-%% Find dFoverF for each trial
+%% Find dFoverF for each trial for F1 iti
 
-dF_data = bsxfun(@minus,data_reg, F1);
-clear data_reg
-dFoverF_data = bsxfun(@rdivide, dF_data, F1);
-max_dF = max(dFoverF_data,[],3);
-figure; imagesq(max_dF); colormap(gray)   
+dF1_data = bsxfun(@minus,data_reg, F1);
+dF1overF1_data = bsxfun(@rdivide, dF1_data, F1);
+max_dF1 = max(dF1overF1_data,[],3);
+figure; imagesq(max_dF1); colormap(gray)   
 
+save 'dF1_data.mat' dF1_data
 %%
 %using Retinotopy_V1 ROI mask, find ROIs in flashingstim dataset
-data_TC_FS = stackGetTimeCourses(dFoverF_data,mask_cell);
+data_TC_FS = stackGetTimeCourses(dF1overF1_data,mask_cell);
 figure; tcOffsetPlot(data_TC_FS)
 
 %save data_reg and ROI timecourses
-save 'timecourses' data_TC_FS;
+save 'timecourses.mat' data_TC_FS;
 
 %%
 %plot data_TC_FS for just cells that have the preferred retinotopy
