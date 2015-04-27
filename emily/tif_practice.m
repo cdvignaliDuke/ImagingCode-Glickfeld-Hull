98cb1390e577030561e2aa3795744e1457ad4302
Emily Burke <burke.em@husky.neu.edu>
2015-04-27 11:43:03 -0400

    practicing tiff code

---
+++ emily/tif_practice.m
@@ -0,0 +1,13 @@
+%%Read each image in a tif
+s1 = imread('Substack.tif', 1);
+s2 = imread('Substack.tif', 2);
+s3 = imread('Substack.tif', 3);
+s4 = imread('Substack.tif', 4);
+
+%%View each image
+imshow(s1)
+imshow(s2)
+imshow(s3)
+imshow(s4)
+
+%%
