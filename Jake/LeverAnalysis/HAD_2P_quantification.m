d8fd4de5732a3fa955e7801e0aba1348dc207c67
Lindsey Glickfeld <llglick@gmail.com>
2015-07-12 19:57:34 -0400

    Outputs figs of FOV by condition

--- Jake/LeverAnalysis/HAD_2P_quantification.m
+++ Jake/LeverAnalysis/HAD_2P_quantification.m
@@ -14,14 +14,20 @@ for id = 1:size(date_mat,1)
     dest =  fullfile(out_path,run_name);
     load([dest '_press_resp_by_hold.mat']);
     load([dest '_release_resp_by_outcome.mat']);
+    load([dest '_ROI_TCs.mat']);
+    load([dest '_ICs.mat']);
     
     nCells = size(success_movie,2);
-    success_base = squeeze(mean(success_movie(:,:,1:pre_release_frames),3));
-    success_resp = squeeze(mean(success_movie(:,:,pre_release_frames+1:pre_release_frames+2),3));
+    
+    base_release_window = 1:pre_release_frames;
+    resp_release_window = pre_release_frames+1:pre_release_frames+2;
+
+    success_base = squeeze(mean(success_movie(:,:,base_release_window),3));
+    success_resp = squeeze(mean(success_movie(:,:,resp_release_window),3));
     [success_h, success_p] = ttest(success_base, success_resp, 'dim', 1, 'tail', 'both');
     
-    fail_base = squeeze(mean(fail_movie(:,:,1:pre_release_frames),3));
-    fail_resp = squeeze(mean(fail_movie(:,:,pre_release_frames+1:pre_release_frames+2),3));
+    fail_base = squeeze(mean(fail_movie(:,:,base_release_window),3));
+    fail_resp = squeeze(mean(fail_movie(:,:,resp_release_window),3));
     [fail_h, fail_p] = ttest(fail_base, fail_resp, 'dim', 1, 'tail', 'both');
     
     success_resp_cells = find(success_h);
@@ -38,8 +44,11 @@ for id = 1:size(date_mat,1)
     
     [fail_v_success_h, fail_v_success_p] = ttest(fail_resp_avg(1,fail_and_success_cells),success_resp_avg(1,fail_and_success_cells), 'tail', 'left');
     
-    press_base = squeeze(mean(press_long_movie(:,:,1:pre_press_frames-5),3));
-    press_resp = squeeze(mean(press_long_movie(:,:,pre_press_frames-1:pre_press_frames),3));
+    base_press_window = 1:pre_press_frames-5;
+    resp_press_window = pre_press_frames-1:pre_press_frames;
+    
+    press_base = squeeze(mean(press_long_movie(:,:,base_press_window),3));
+    press_resp = squeeze(mean(press_long_movie(:,:,resp_press_window),3));
     [press_h, press_p] = ttest(press_base, press_resp, 'dim', 1, 'tail', 'both');
     
     press_resp_cells = find(press_h);
@@ -50,5 +59,63 @@ for id = 1:size(date_mat,1)
     press_notsuccess_cells = press_resp_cells(find(ismember(press_resp_cells, success_resp_cells)==0));
     success_notpress_cells = success_resp_cells(find(ismember(success_resp_cells, press_resp_cells)==0));
     
-    noresponse_cells = find(sum(success_h,fail_h,press_h)==0);
+    noresponse_cells = find((success_h+fail_h+press_h)==0);
+    
+    save([dest '_cell_categories.mat'], 'success_resp_cells', 'fail_resp_cells', 'press_resp_cells','noresponse_cells', 'fail_only_cells', 'success_only_cells', 'fail_and_success_cells', 'press_and_success_cells', 'press_notsuccess_cells', 'success_notpress_cells');
+    save([dest '_cell_resp.mat'], 'base_release_window', 'resp_release_window', 'base_press_window', 'resp_press_window', 'press_base', 'press_resp', 'success_base', 'success_resp', 'fail_base', 'fail_resp');
+    
+    figure;
+    all_resp = [success_resp_avg fail_resp_avg press_resp_avg];
+    cmax = max(all_resp,[],2);
+    cmin = min(all_resp,[],2);
+    success_mask = mask_final;
+    sz = size(sm);
+    for ic = 1:nCells
+        if success_h(ic)
+            success_mask(find(success_mask==ic))=success_resp_avg(1,ic);
+        else
+            success_mask(find(success_mask==ic))=0;
+        end
+    end
+    success_mask = reshape(success_mask,[sz(1) sz(2)]);
+    subplot(1,3,1)
+    imagesc(success_mask)
+    clim([cmin cmax])
+    title('success')
+    
+    fail_mask = mask_final;
+    sz = size(sm);
+    for ic = 1:nCells
+        if fail_h(ic)
+            fail_mask(find(fail_mask==ic))=fail_resp_avg(1,ic);
+        else
+            fail_mask(find(fail_mask==ic))=0;
+        end
+    end
+    fail_mask = reshape(fail_mask,[sz(1) sz(2)]);
+    subplot(1,3,2)
+    imagesc(fail_mask)
+    clim([cmin cmax])
+    title('failure')
+    
+    press_mask = mask_final;
+    sz = size(sm);
+    for ic = 1:nCells
+        if press_h(ic)
+            press_mask(find(press_mask==ic))=press_resp_avg(1,ic);
+        else
+            press_mask(find(press_mask==ic))=0;
+        end
+    end
+    press_mask = reshape(press_mask,[sz(1) sz(2)]);
+    subplot(1,3,3)
+    imagesc(press_mask)
+    clim([cmin cmax])
+    title('press')
+    
+    suptitle([mouse ' ' date ' cell responses']);
+    print([dest '_cell_responses_FOV.eps'], '-depsc');
+    print([dest '_cell_responses_FOV.pdf'], '-dpdf');
+    
+    save([dest '_ROI_TCs.mat'], 'data_tc', 'mask_final', 'sz');
 end
