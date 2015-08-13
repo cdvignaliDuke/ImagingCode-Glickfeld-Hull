099bb26930ca9de717dc6880059e5ffd59ab6842
ashley <wilson@neuro.duke.edu>
2015-08-13 11:37:46 -0400

    new eye dataset added - aw

--- ashley/eye tracking/eyeTracking_processing.m
+++ ashley/eye tracking/eyeTracking_processing.m
@@ -1,26 +1,28 @@
 SubNum = '614';
-date = '150623';
-time = '1235';
-ImgFolder = '005';
+date = '150811';
+time = '1115';
+ImgFolder = '001';
 mouse = 'AW14';
-fName = '005_000_000';
+fName = '001_000_000';
 
 % % load MWorks file
 % CD = ['Z:\data\' mouse '\mworks\' date];
-% cd(CD);
-% mworks = ['data-' 'i' SubNum '-' date '-' time]; 
-% load (mworks);
+CD = ['Y:\home\andrew\Behavior\Data'];
+cd(CD);
+mworks = ['data-' 'i' SubNum '-' date '-' time]; 
+load (mworks);
 
 % Set current directory to crash folder
-CD = ['Z:\data\' mouse '\two-photon imaging\' date '\' ImgFolder];
+directory = 'Y:\home\ashley';
+CD = [directory '\data\' mouse '\eye tracking\' date '\' ImgFolder];
 cd(CD);
 
 %% 
 eyedata = sbxeyemotion([fName '_eye.mat']);
 eyeArea = [eyedata.Area];
-eyeCentroid = 
+eyeCentroid = reshape([eyedata.Centroid],2,length(eyeArea));
 
 %%
 load([fName '_eye.mat'])
 eyemov = squeeze(data);
-writetiff(eyemov,'eyemovie')
+writetiff(eyemov,'eyemovie');
