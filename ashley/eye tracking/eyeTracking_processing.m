2e385ce389d7b658d75b1de3e143d6c4365c71f8
Lindsey Glickfeld <llglick@gmail.com>
2015-08-12 21:12:08 -0400

    Fixed  indexing errors - LG

--- ashley/eye tracking/eyeTracking_processing.m
+++ ashley/eye tracking/eyeTracking_processing.m
@@ -4,7 +4,7 @@ ImgFolder = '004';
 mouse = 'AW14';
 fName = '004_000_000';
 
-fnout = '\\CRASH.dhe.duke.edu\data\home\lindsey
+fnout = ['Z:\home\lindsey\Analysis\Behavior\EyeTracking\' mouse '-' date '-' ImgFolder];
 % % load MWorks file
 % CD = ['Z:\data\' mouse '\mworks\' date];
 % cd(CD);
@@ -73,6 +73,10 @@ for itrial = 1:ntrials
     centroid_mat_down(:,:,itrial) = Centroid(1+leverDownFrame(itrial)-prepush_frames:leverDownFrame(itrial)+postpush_frames,:);
 end
 
+set(0,'defaultfigurepaperorientation','portrait');
+set(0,'defaultfigurepapersize',[8.5 11]);
+set(0,'defaultfigurepaperposition',[.25 .25 [8.5 11]-0.5]);
+
 %plot change in eye area
 figure;
 tt = 1-prepush_frames:postpush_frames;
@@ -89,6 +93,7 @@ shadedErrorBar(tt,squeeze(nanmean(centroid_mat_down(:,2,:),3)), squeeze(nanstd(c
 xlabel('Frames')
 ylabel('Eye position- Vertical')
 suptitle('Align to lever down')
+print([fnout '_avg_pressalign.pdf'], '-dpdf');
 
 %plot change in eye area by block type
 b1Ix = find(cell2mat(input.tBlock2TrialNumber)==0);
@@ -115,40 +120,41 @@ shadedErrorBar(tt,squeeze(nanmean(centroid_mat_down(:,2,b2Ix),3)), squeeze(nanst
 xlabel('Frames')
 ylabel('Eye position- Vertical')
 suptitle('Align to lever down')
+print([fnout '_avg_pressalign_AV.pdf'], '-dpdf');
 
 %change with target
 targetOnFrame = cell2mat_padded(input.cTargetOn);
-targetInd = find(targetOnFrame>0);
-targetOnFrame(find(targetOnFrame==0)) = [];
-targetTrials = size(targetOnFrame,1);
-area_mat_target = zeros(prepush_frames+postpush_frames, targetTrials);
-centroid_mat_target = zeros(prepush_frames+postpush_frames,2, targetTrials);
-for itrial = 1:targetTrials
-    area_mat_target(:,itrial) = Area(1+targetOnFrame(itrial)-prepush_frames:targetOnFrame(itrial)+postpush_frames,:);
-    centroid_mat_target(:,:,itrial) = Centroid(1+targetOnFrame(itrial)-prepush_frames:targetOnFrame(itrial)+postpush_frames,:);
+area_mat_target = zeros(prepush_frames+postpush_frames, ntrials);
+centroid_mat_target = zeros(prepush_frames+postpush_frames,2, ntrials);
+for itrial = 1:ntrials
+    if targetOnFrame(itrial)>0
+        area_mat_target(:,itrial) = Area(1+targetOnFrame(itrial)-prepush_frames:targetOnFrame(itrial)+postpush_frames,:);
+        centroid_mat_target(:,:,itrial) = Centroid(1+targetOnFrame(itrial)-prepush_frames:targetOnFrame(itrial)+postpush_frames,:);
+    else
+        area_mat_target(:,itrial) = NaN(prepush_frames+postpush_frames, 1);
+        centroid_mat_target(:,:,itrial) = NaN(prepush_frames+postpush_frames,2, 1);
+    end
 end
 
 %plot change in eye area
 figure;
 tt = 1-prepush_frames:postpush_frames;
 subplot(3,1,1)
-shadedErrorBar(tt,nanmean(area_mat_target,2), nanstd(area_mat_target,[],2)./sqrt(targetTrials));
+shadedErrorBar(tt,nanmean(area_mat_target,2), nanstd(area_mat_target,[],2)./sqrt(ntrials));
 xlabel('Frames')
 ylabel('Eye area')
 subplot(3,1,2)
-shadedErrorBar(tt,squeeze(nanmean(centroid_mat_target(:,1,:),3)), squeeze(nanstd(centroid_mat_target(:,1,:),[],3))./sqrt(targetTrials));
+shadedErrorBar(tt,squeeze(nanmean(centroid_mat_target(:,1,:),3)), squeeze(nanstd(centroid_mat_target(:,1,:),[],3))./sqrt(ntrials));
 xlabel('Frames')
 ylabel('Eye position- Horizontal')
 subplot(3,1,3)
-shadedErrorBar(tt,squeeze(nanmean(centroid_mat_target(:,2,:),3)), squeeze(nanstd(centroid_mat_target(:,2,:),[],3))./sqrt(targetTrials));
+shadedErrorBar(tt,squeeze(nanmean(centroid_mat_target(:,2,:),3)), squeeze(nanstd(centroid_mat_target(:,2,:),[],3))./sqrt(ntrials));
 xlabel('Frames')
 ylabel('Eye position- Vertical')
 suptitle('Align to target')
+print([fnout '_avg_targetalign.pdf'], '-dpdf');
 
 %plot change in eye area by block type
-b1Ix = intersect(targetInd, find(cell2mat(input.tBlock2TrialNumber)==0));
-b2Ix = intersect(targetInd, find(cell2mat(input.tBlock2TrialNumber)));
-
 figure;
 tt = 1-prepush_frames:postpush_frames;
 subplot(3,1,1)
@@ -170,3 +176,4 @@ shadedErrorBar(tt,squeeze(nanmean(centroid_mat_target(:,2,b2Ix),3)), squeeze(nan
 xlabel('Frames')
 ylabel('Eye position- Vertical')
 suptitle('Align to target')
+print([fnout '_avg_targetalign_AV.pdf'], '-dpdf');
