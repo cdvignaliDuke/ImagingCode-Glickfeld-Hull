6ed24d29ec35f1d987499529b145204e409feb6c
Lindsey Glickfeld <llglick@gmail.com>
2015-09-01 13:51:51 -0400

    AW imaging datasets for AW13 and AW14

---
+++ lindsey/AW calcium imaging/AWdatasets.m
@@ -0,0 +1,53 @@
+%% AW13 150504
+SubNum = '613';
+mouse = 'AW13';
+date = '150504';
+time_mat = ['1425'; '1441'; '1457'];
+runs = ['002'; '003'; '004'];
+nrun = size(runs,1);
+frame_rate = 30;
+
+%% AW13 150508
+SubNum = '613';
+mouse = 'AW13';
+date = '150508';
+time_mat = ['1413'; '1430'];
+runs = ['001'; '002'];
+nrun = size(runs,1);
+frame_rate = 30;
+
+%% AW13 150511
+SubNum = '613';
+mouse = 'AW13';
+date = '150511';
+time_mat = ['1420'; '1437'; '1453'];
+runs = ['001'; '002'; '003'];
+nrun = size(runs,1);
+frame_rate = 30;
+
+%% AW14 150623
+SubNum = '614';
+mouse = 'AW14';
+date = '150623';
+time_mat = ['1142'; '1158'; '1217'];
+runs = ['002'; '003'; '004'];
+nrun = size(runs,1);
+frame_rate = 30;
+
+%% AW14 150626
+SubNum = '614';
+mouse = 'AW14';
+date = '150626';
+time_mat = ['1458'; '1514'; '1532'];
+runs = ['001'; '002'; '003'];
+nrun = size(runs,1);
+frame_rate = 30;
+
+%% AW14 150701
+SubNum = '614';
+mouse = 'AW14';
+date = '150701';
+time_mat = ['1556'; '1615'; '1633'];
+runs = ['002'; '003'; '004'];
+nrun = size(runs,1);
+frame_rate = 30;
