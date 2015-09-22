df471a00aa962f2b62bda66fa1c7ecbb0ce862bb
ashley <wilson@neuro.duke.edu>
2015-09-22 11:40:45 -0400

    2P imaging datasets - AW

---
+++ ashley/dataset organization/awFSAVdatasets.m
@@ -0,0 +1,72 @@
+%% AW13 150504
+expt(1).SubNum = '613';
+expt(1).mouse = 'AW13';
+expt(1).date = '150504';
+expt(1).time_mat = ['1425'; '1441';'1457'];
+expt(1).runs = ['002'; '003'; '004'];
+expt(1).nrun = size(expt(1).runs,1);
+expt(1).frame_rate = 30;
+expt(1).folder = 'two-photon imaging';
+expt(1).catch = 0;
+expt(1).dirtuning = '006';
+
+%% AW13 150508
+expt(2).SubNum = '613';
+expt(2).mouse = 'AW13';
+expt(2).date = '150508';
+expt(2).time_mat = ['1413'; '1430'; '1446'];
+expt(2).runs = ['001'; '002'; '003'];
+expt(2).nrun = size(expt(1).runs,1);
+expt(2).frame_rate = 30;
+expt(2).folder = 'two-photon imaging';
+expt(2).catch = 1;
+expt(2).dirtuning = '006';
+
+%% AW13 150511
+expt(3).SubNum = '613';
+expt(3).mouse = 'AW13';
+expt(3).date = '150511';
+expt(3).time_mat = ['1420'; '1437'; '1453'];
+expt(3).runs = ['001'; '002'; '003'];
+expt(3).nrun = size(expt(1).runs,1);
+expt(3).frame_rate = 30;
+expt(3).folder = 'two-photon imaging';
+expt(3).catch = 0;
+expt(3).dirtuning = '005';
+
+%% AW14 150623
+expt(4).SubNum = '614';
+expt(4).mouse = 'AW14';
+expt(4).date = '150623';
+expt(4).time_mat = ['1142'; '1158'; '1217'];
+expt(4).runs = ['002'; '003'; '004'];
+expt(4).nrun = size(expt(1).runs,1);
+expt(4).frame_rate = 30;
+expt(4).folder = 'two-photon imaging';
+expt(4).catch = 0;
+expt(4).dirtuning = '006';
+
+%% AW14 150626
+expt(5).SubNum = '614';
+expt(5).mouse = 'AW14';
+expt(5).date = '150626';
+expt(5).time_mat = ['1458'; '1514'; '1532'];
+expt(5).runs = ['001'; '002'; '003'];
+expt(5).nrun = size(expt(1).runs,1);
+expt(5).frame_rate = 30;
+expt(5).folder = 'two-photon imaging';
+expt(5).catch = 0;
+expt(5).dirtuning = '005';
+
+%% AW14 150701
+expt(6).SubNum = '614';
+expt(6).mouse = 'AW14';
+expt(6).date = '150701';
+expt(6).time_mat = ['1556'; '1615'; '1633'];
+expt(6).runs = ['002'; '003'; '004'];
+expt(6).nrun = size(expt(1).runs,1);
+expt(6).frame_rate = 30;
+expt(6).folder = 'two-photon imaging';
+expt(6).catch = 1;
+expt(6).dirtuning = '006';
+
