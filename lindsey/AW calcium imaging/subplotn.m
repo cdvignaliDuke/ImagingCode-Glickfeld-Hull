fb062f46d75b8860a0ac56a64b5feeee9c7fd2fc
Lindsey Glickfeld <llglick@gmail.com>
2016-03-27 18:46:16 -0400

    edit for small numbers- LG

--- lindsey/AW calcium imaging/subplotn.m
+++ lindsey/AW calcium imaging/subplotn.m
@@ -1,9 +1,14 @@
 function [n, n2] = subplotn(ntot);
 %output minimum subplot dimensions given n conditions
-    n = ceil(sqrt(ntot));
-    if n^2-n >= ntot
-        n2 = n-1;
+    if ntot<4
+        n = ntot;
+        n2 = 1;
     else
-        n2 =n;
+        n = ceil(sqrt(ntot));
+        if n^2-n >= ntot
+            n2 = n-1;
+        else
+            n2 =n;
+        end
     end
 end
