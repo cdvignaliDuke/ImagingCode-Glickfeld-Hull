642a903e6e6eab98fc9069ff6a94f76192d32e02
Lindsey Glickfeld <llglick@gmail.com>
2016-04-01 11:32:36 -0400

    Flashing Stim widefield anaylsis- LG

---
+++ lindsey/WFanalysis_code/FSWFdatasets.m
@@ -0,0 +1,33 @@
+data_root = 'Z:\home\lindsey\Data\Widefield_images';
+anal_root = 'Z:\home\lindsey\Analysis\Widefield_imaging';
+expt_name = 'FS5';
+%% i538 160314
+expt(1).SubNum = '538';
+expt(1).date = '160314';
+expt(1).img_loc  = 'V1';
+expt(1).obj  = 16;
+expt(1).time_mat = '1533';
+expt(1).runs = 2;
+expt(1).frame_rate = 30;
+expt(1).g_ind = 4;
+
+%% i540 160316
+expt(2).SubNum = '540';
+expt(2).date = '160316';
+expt(2).img_loc  = 'V1';
+expt(2).obj  = 16;
+expt(2).time_mat = '1728';
+expt(2).runs = 2;
+expt(2).frame_rate = 30;
+expt(2).g_ind = 4;
+
+%% i540 160315
+expt(3).SubNum = '540';
+expt(3).date = '160315';
+expt(3).img_loc  = 'HV';
+expt(3).obj  = 5;
+expt(3).time_mat = '0956';
+expt(3).runs = 2;
+expt(3).frame_rate = 30;
+expt(3).g_ind = 4;
+
