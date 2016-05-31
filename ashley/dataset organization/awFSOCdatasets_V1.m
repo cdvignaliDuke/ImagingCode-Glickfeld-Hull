5158a9ba78a8c0bb164824c16bcfbe8a3c5e6396
ashley <wilson@neuro.duke.edu>
2016-05-31 09:32:47 -0400

    new datasests - AW

---
+++ ashley/dataset organization/awFSOCdatasets_V1.m
@@ -0,0 +1,16 @@
+%% AW13 160512
+expt(1).SubNum = '617';
+expt(1).mouse = 'AW17';
+expt(1).date = '160512';
+expt(1).img_loc  = {'V1';'L2/3'};
+expt(1).img_strct  = {'cells'};
+expt(1).time_mat = ['1042'; '1058';'1116'];
+expt(1).runs = ['002'; '003'; '004'];
+expt(1).nrun = size(expt(1).runs,1);
+expt(1).frame_rate = 30;
+expt(1).folder = 'two-photon imaging';
+expt(1).catch = 0;
+expt(1).dirtuning = '007';
+expt(1).dirtuning_time = '1137';
+expt(1).rettuning = {'005';'1132'};
+expt(1).motionTestBimodal = 0;
