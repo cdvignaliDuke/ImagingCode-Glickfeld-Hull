b68c2b3059948c5b750c9c42e659648243513817
NUKE\ziye <dec.ziye@gmail.com>
2016-09-23 17:40:10 -0400

    imagesc command type   -ZX

--- Jake/LeverAnalysis/Two-photon/prepare_movie_for_analysis_2P.m
+++ Jake/LeverAnalysis/Two-photon/prepare_movie_for_analysis_2P.m
@@ -52,7 +52,7 @@ end
 %add in an if statement to use the first movie to register the second one.
 %Save the data_avg
 data_avg = mean(data_sub(:,:,90:190),3);
-figure; imagesq(data_avg); colormap(gray)
+figure; imagesc(data_avg); colormap(gray)
 [out data_reg] = stackRegister(data_sub, data_avg);
 clear data_sub
 
