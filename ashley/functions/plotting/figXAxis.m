a22b906fb28b843094a24bd2545ac9fbe53e184a
ashley <wilson@neuro.duke.edu>
2017-03-29 15:59:32 -0400

    fixed bug

--- ashley/functions/plotting/figXAxis.m
+++ ashley/functions/plotting/figXAxis.m
@@ -1,11 +1,11 @@
 function figXAxis(fig,x_name,x_limit,varargin)
-
 xlabel(x_name);
 xlim(x_limit);
-if length(varargin) > 2
-    x_tick = varargin{3};
-    if length(varargin) > 3
-        x_tick_label = varargin{4};
+if length(varargin) > 0
+    x_tick = varargin{1};
+    fig.XTick = x_tick;
+    if length(varargin) > 1
+        x_tick_label = varargin{2};
         fig.XTickLabel = x_tick_label;
     else
         fig.XTickLabel = x_tick;
--- ashley/functions/plotting/figYAxis.m
+++ ashley/functions/plotting/figYAxis.m
@@ -2,11 +2,11 @@ function figYAxis(fig,y_name,y_limit,varargin)
 
 ylabel(y_name);
 ylim(y_limit);
-if length(varargin) > 2
-    y_tick = varargin{3};
+if length(varargin) > 0
+    y_tick = varargin{1};
     fig.YTick = y_tick;
-    if length(varargin) > 3
-        y_tick_label = varargin{4};
+    if length(varargin) > 1
+        y_tick_label = varargin{2};
         fig.YTickLabel = y_tick_label;
     else
         fig.YTickLabel = y_tick;
