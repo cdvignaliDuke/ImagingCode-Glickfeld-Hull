6f88a3b656fa3de9a15e350c4fcebce23867b4c2
Lindsey Glickfeld <llglick@gmail.com>
2018-02-21 09:57:58 -0500

    function for performing svm with bootstrap- LG

---
+++ lindsey/2Panalysis_code/svm_withboot.m
@@ -0,0 +1,15 @@
+function pct_corr = svm_withboot(data, groups, nboot)
+% svm applied to binary data- train and test on holdout data (50%)
+% data: n x p matrix where n is trials/observation and p are predictors
+% groups: n x 1 matrix to identify trial category
+% nboot: number of repeats of train/test outcome
+
+pct_corr = nan(1,nboot);
+for i = 1:nboot
+    [train, test] = crossvalind('holdOut',groups);
+    cp = classperf(groups);
+    svmStruct = fitcsvm(data(train,:),groups(train));
+    out = predict(svmStruct, data(test,:));
+    pct_corr(1,i) = 1-sum(abs(groups(test,:)-out))./length(test);
+end
+
