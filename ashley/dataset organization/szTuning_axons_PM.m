31959491c323f936501b763da261ba061f86d26d
Lindsey Glickfeld <llglick@gmail.com>
2018-09-13 17:29:52 -0400

    Updated size tuning for axons- LG

---
+++ ashley/dataset organization/szTuning_axons_PM.m
@@ -0,0 +1,28 @@
+params.frameRate = 15.49;
+params.nBaselineMs = 1000;
+params.nPostStimMs = 2000;
+params.nFramesVisDelay_VSR = 4;
+params.motionCutoff = 0.2;
+params.itiTimeS = 2;
+
+%% 1200 180912
+expt(1).mouse = '1200';
+expt(1).date = '180913';
+expt(1).img_loc  = {'PM';'L23'};
+expt(1).z = -150;
+expt(1).img_strct  = {'axons'};
+expt(1).indicator = {'tg';'GCaMP6s'};
+expt(1).label = {'None'};
+expt(1).labelFolder = '';
+expt(1).drug = '';
+expt(1).matchExptDate = nan;
+expt(1).retinotopyFolder = {'004'};
+expt(1).retinotopyTime = {'1443'};
+expt(1).sizeTuningFolder = {'005';'007'};
+expt(1).sizeTuningTime = {'1526';'1600'};
+expt(1).sizeTuningMaxTrials = {'100';''};
+expt(1).sizeTuningTimeFromDrugMin = {''};
+expt(1).areaBorders = 0;
+expt(1).regImgStartFrame = 7103;
+expt(1).saveLoc = 'ashley';
+
